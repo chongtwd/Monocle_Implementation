c0dc8c7ed26c35a2bb47cc0f86555e4e7109ff59
David Chong <sumbrella2001@yahoo.com.sg>
2021-04-22 20:13:56 +0100

    Implemented Prims for MST

---
+++ Prims.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 20:37:47 2021
+
+@author: David Chong Tian Wei
+"""
+import numpy as np
+
+class BinaryHeap:
+    """
+    Implementation of a binary heap using list as underlying storage
+    """
+    def __init__(self, values=[], comparator = lambda x, y : x < y, identifier = lambda x : x):
+        """
+        The binary heap may be instantiated with a comparator to enable
+        specification as max or min heap as well as to allow comparison
+        of objects
+        """
+        self.store = []
+        self.comparator = comparator
+        self.identifier = identifier
+        self.map = {}
+        for x in values:
+            self.add(x)
+        
+    def __len__(self):
+        return len(self.store)
+    
+    def add(self, x):
+        """
+        When we add an element to the heap the rest of the list is already
+        in the form of a heap, so we need only bubble the added element
+        until the heap property is satisfied across the whole list
+        """
+        self.store.append(x)
+        self.map[self.identifier(x)] = len(self.store) - 1
+        i = len(self.store) - 1
+        p = (i-1) // 2
+        while p >= 0:
+            if self.comparator(self.store[i], self.store[p]):
+                self.__swap_nodes(self.store[i], self.store[p])
+                i = p
+                p = (i-1) // 2
+            else:
+                return
+    
+    def get(self):
+        """
+        When we remove the most extreme element, it will always be the root
+        of the tree. We can then move the last element to the root without
+        breaking the tree structure and then heapify to re-establish the heap
+        property
+        """
+        output = self.store[0]
+        self.remove(self.identifier(self.store[0]))
+        i = 0
+        j = self.__compare_children(i)
+        while i != j:
+            self.__swap_nodes(self.store[i], self.store[j])
+            i = j
+            j = self.__compare_children(i)
+            
+        return output
+    
+    def peek(self):
+        return self.store[0]
+    
+    def remove(self, x):
+        i = self.map[x]
+        self.__swap_nodes(self.store[i], self.store[-1])
+        self.store = self.store[:-1]
+        del self.map[x]
+        j = self.__compare_children(i)
+        while i != j:
+            self.__swap_nodes(self.store[i], self.store[j])
+            i = j
+            j = self.__compare_children(i)
+    
+    def update(self, k, v):
+        i = self.map[k]
+        if self.comparator(v, self.store[i]):
+            self.remove(k)
+            self.add(v)
+    
+    def __compare_children(self, i):
+        if 2*i + 2 < len(self.store):
+            if self.comparator(self.store[2*i+1], self.store[2*i+2]):
+                if self.comparator(self.store[i], self.store[2*i+1]):
+                    return i
+                else:
+                    return 2*i + 1
+            else:
+                if self.comparator(self.store[i], self.store[2*i+2]):
+                    return i
+                else:
+                    return 2*i+2
+        elif 2*i + 1 < len(self.store):
+            if self.comparator(self.store[i], self.store[2*i+1]):
+                return i
+            else:
+                return 2*i + 1
+        else:
+            return i
+    
+    def __swap_nodes(self, x, y):
+        i = self.map[self.identifier(x)]
+        j = self.map[self.identifier(y)]
+        temp = self.store[i]
+        self.store[i] = self.store[j]
+        self.store[j] = temp
+        self.map[self.identifier(x)] = j
+        self.map[self.identifier(y)] = i
+        
+def Prims(G, inf=999999999):
+    """
+    Expect G to be an n x n ajacency matrix of an undirected graph
+    """
+    g = []
+    for i in range(G.shape[0]):
+        edges = []
+        for j in range(0, G.shape[0]):
+            if j == i:
+                edges.append([j, inf])
+            else:
+                edges.append([j, G[i,j]])
+        g.append(edges)
+    Q = BinaryHeap([[i, None, inf] for i in range(len(g))], lambda x,y : x[2] < y[2], lambda x : x[0])
+    mst = []
+    reached = {}
+    for i in range(len(Q)):
+        reached[i] = False
+    i = 0
+    while len(Q) > 1:
+        reached[i] = True
+        for x in g[i]:
+            if not reached[x[0]]:
+                Q.update(x[0], [x[0], i, x[1]])
+        v, u, w = Q.get()
+        mst.append((u, v, w))
+        i = v
+    return mst
---
+++ Prims_Test.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 21 23:40:24 2021
+
+@author: David Chong Tian Wei
+"""
+import pygraphviz as pgv
+import numpy as np
+from Prims import Prims
+
+def generate_graph(n=20):
+    G = np.zeros(shape=(n,n))
+    for i in range(n):
+        for j in range(i+1,n):
+            G[i,j] = np.random.randint(1,10)
+            G[j,i] = G[i,j]
+    return G
+
+g = generate_graph()
+
+G = pgv.AGraph(strict=True, directed=False)
+G.add_nodes_from(range(g.shape[0]))
+for i in range(g.shape[0]):
+    for j in range(i+1, g.shape[0]):
+        G.add_edge(str(i), str(j), label=str(g[i,j]))
+G.layout(prog="dot")
+G.draw("test.png")
+
+
+mst = Prims(g)
+
+for x in mst:
+    e = G.get_edge(str(x[0]), str(x[1]))
+    e.attr["color"] = "red"
+    e.attr["fontcolor"] = "red"
+G.draw("test_mst.png")
